c4c67a9b537b5010ae385b63fcf830f8747ff25d
Jennifer Roques <jenniferroques@gmail.com>
2021-12-13 21:05:31 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+const submitData = (name, email) => {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    };
+
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(newId)
+        .catch(response => document.body.innerHTML = 'Unauthorized Access')
+};
+
+
+function newId(object) {
+    document.body.innerHTML = object.id
+}
